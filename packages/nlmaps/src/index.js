e8b48bd54228c85f63a5c46e67d3b91dfef3faa8
Hans Fast <hans@surfacemarkup.net>
2017-05-29 09:49:59 +0200

    nlmaps super-package works

--- packages/nlmaps/src/index.js
+++ packages/nlmaps/src/index.js
@@ -16,32 +16,73 @@ let nlmaps = {
 };
 
 let mapdefaults = {
-
+  layer: 'standaard',
+  center: {
+    latitude: 52,
+    longitude: 5
+  },
+  zoom: 10
 };
 
 
-function mapLibDefined() {
+function testWhichLib() {
+  let defined = [];
   if (typeof L === 'object') {
-    return 'L';
+    defined.push('leaflet');
   
-  };
+  }
   if (typeof google === 'object' && typeof google.maps === 'object'){
-    return 'google.maps';
+    defined.push('googlemaps');
   }
   if (typeof ol === 'object') {
-    return 'ol';
+    defined.push('openlayers');
+  }
+
+  if( defined.length > 1 ) {
+    return 'too many libs';
+  } else if ( defined.length === 0 ) {
+    return 'too few libs';
+  } else {
+    return defined[0];
   }
 };
 
+function initMap(lib, opts){
+  let map;
+  switch (lib) {
+    case 'leaflet':
+      map = L.map(opts.target).setView([opts.center.latitude, opts.center.longitude], opts.zoom);
+      break;
+    case 'googlemaps':
+      map = new google.maps.Map(document.getElementById(opts.target), {
+        center: {lat: opts.center.latitude, lng: opts.center.longitude},
+        zoom: opts.zoom
+      });
+      break;
+    case 'openlayers':
+      map = new ol.Map({
+        view: new ol.View({
+          center: ol.proj.fromLonLat([opts.center.longitude, opts.center.latitude]),
+          zoom: opts.zoom
+        }),
+        target: opts.target
+      });
 
-function optsForLib(opts, lib) {
+  }
+  return map;
 
 };
 
-nlmaps.createMap = function(options) {
-  
-
-
+nlmaps.createMap = function(useropts = {}) {
+  const opts = Object.assign({}, mapdefaults, useropts);
+  let lib = testWhichLib();
+  if (lib === 'too many libs' || lib === 'too few libs') {
+    throw({message:'one and only one map library can be defined.'});
+    return;
+  }
+  let map = initMap(lib, opts);
+  nlmaps[lib].bgLayer(opts.layer);
+  return map;
 };
 
 export default nlmaps;

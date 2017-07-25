c3b4cc23c3bec2e6172e0004c691d611216a18cf
Hans Fast <hans@surfacemarkup.net>
2017-07-25 12:39:56 +0200

    implement a proof-of-concept control for Leaflet

--- packages/nlmaps-leaflet/src/index.js
+++ packages/nlmaps-leaflet/src/index.js
@@ -22,6 +22,35 @@ L.nlmapsBgLayer = function (options, source) {
   return new L.NlmapsBgLayer(options, source);
 };
 
+L.Control.GeoLocatorControl = L.Control.extend({
+  options: {
+    position: 'topleft'
+  },
+  initialize: function (options) {
+            // set default options if nothing is set (merge one step deep)
+            for (var i in options) {
+                if (typeof this.options[i] === 'object') {
+                    L.extend(this.options[i], options[i]);
+                } else {
+                    this.options[i] = options[i];
+                }
+            }
+  },
+
+  onAdd: function(map){
+    let div = L.DomUtil.create('div');
+    div.innerHTML = 'hi there world';
+    L.DomEvent.on(div, 'click', this.options.geolocator)
+    return div;
+  
+  },
+  onRemove: function(map){}
+})
+
+L.geoLocatorControl = function(geolocator){
+  return new L.Control.GeoLocatorControl({geolocator:geolocator});
+}
+
 
 }
 
@@ -31,4 +60,12 @@ function bgLayer(name) {
   }
 }
 
-export { bgLayer };
+function geoLocatorControl(geolocator) {
+  if (typeof L !== 'undefined' && typeof L === 'object') {
+    return L.geoLocatorControl(geolocator)
+  }
+
+  
+}
+
+export { bgLayer, geoLocatorControl };

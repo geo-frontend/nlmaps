d806a805f7e5fef931190875d73ab9aca8c1d726
Hans Fast <hans@surfacemarkup.net>
2017-07-25 12:36:20 +0200

    add https to live server in nlmaps package
    for testing geolocation API, need a secure environment.

--- packages/nlmaps/liveserver.js
+++ packages/nlmaps/liveserver.js
@@ -6,6 +6,7 @@ var params = {
     open: false, // When false, it won't load your browser by default. 
     ignore: '', // comma-separated string for paths to ignore 
     //wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec. 
-    logLevel: 2 // 0 = errors only, 1 = some, 2 = lots 
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots 
+    https: 'node_modules/live-server-https'
 };
 liveServer.start(params);

2812cf22062f9eb1724a64f71fbf612cc0fe22f5
Hans Fast <hans@surfacemarkup.net>
2017-07-28 15:51:47 +0200

    build

--- dist/nlmaps.iife.js
+++ dist/nlmaps.iife.js
@@ -611,140 +611,6 @@ var _typeof$2 = typeof Symbol === "function" && _typeof$1(Symbol.iterator) === "
   return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === 'undefined' ? 'undefined' : _typeof$1(obj);
 };
 
-function bgLayer$1() {
-  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'standaard';
-
-  var provider = getProvider$1(name);
-  if ((typeof ol === 'undefined' ? 'undefined' : _typeof$2(ol)) === "object") {
-    return new ol.layer.Tile({
-      source: new ol.source.XYZ({
-        url: provider.url,
-        attributions: [new ol.Attribution({
-          html: provider.attribution
-        })]
-      })
-    });
-  } else {
-    throw 'openlayers is not defined';
-  }
-}
-
-function geoLocatorControl$1(geolocator, map) {
-  var myControlEl = document.createElement('div');
-  myControlEl.id = 'nlmaps-geolocator-control';
-  myControlEl.style.backgroundColor = '#fff';
-  myControlEl.style.cursor = 'pointer';
-  myControlEl.style.boxShadow = '0 1px 5px rgba(0, 0, 0, 0.65)';
-  myControlEl.style.height = '26px';
-  myControlEl.style.width = '26px';
-  myControlEl.style.borderRadius = '26px 26px';
-  myControlEl.innerHTML = geolocator_icon$1;
-  myControlEl.className = 'ol-control';
-  myControlEl.style.right = '.5em';
-  myControlEl.style.top = '.5em';
-
-  myControlEl.addEventListener('click', function (e) {
-    geolocator.start();
-  });
-  function moveMap(d) {
-    var map = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : map;
-
-    var oldZoom = map.getView().getZoom();
-    var view = new ol.View({
-      center: ol.proj.fromLonLat([d.coords.longitude, d.coords.latitude]),
-      zoom: oldZoom
-    });
-    map.setView(view);
-  }
-  geolocator.on('position', function (d) {
-    moveMap(d, map);
-  });
-  var control = new ol.control.Control({ element: myControlEl });
-  return control;
-}
-
-/*parts copied from maps.stamen.com: https://github.com/stamen/maps.stamen.com/blob/master/js/tile.stamen.js
- * copyright (c) 2012, Stamen Design
- * under BSD 3-Clause license: https://github.com/stamen/maps.stamen.com/blob/master/LICENSE
- */
-//https://geodata.nationaalgeoregister.nl/tiles/service/wmts/
-//https://geodata.nationaalgeoregister.nl/luchtfoto/rgb/wmts/
-
-var lufostring$2 = 'luchtfoto/rgb';
-var brtstring$2 = 'tiles/service';
-var servicecrs$2 = '/EPSG:3857';
-var attr$2 = 'Kaartgegevens &copy; <a href="kadaster.nl">Kadaster</a> | <a href="http://www.verbeterdekaart.nl">verbeter de kaart</a>';
-function baseUrl$2(name) {
-  return 'https://geodata.nationaalgeoregister.nl/' + (name === 'luchtfoto' ? lufostring$2 : brtstring$2) + '/wmts/';
-}
-
-function mapLayerName$2(layername) {
-  var name = void 0;
-  switch (layername) {
-    case 'standaard':
-      name = 'brtachtergrondkaart';
-      break;
-    case 'grijs':
-      name = 'brtachtergrondkaartgrijs';
-      break;
-    case 'pastel':
-      name = 'brtachtergrondkaartpastel';
-      break;
-    case 'luchtfoto':
-      name = '2016_ortho25';
-      break;
-    default:
-      name = 'brtachtergrondkaart';
-  }
-  return name;
-}
-
-function makeProvider$2(name, format, minZoom, maxZoom) {
-  var baseurl = baseUrl$2(name);
-  var urlname = mapLayerName$2(name);
-  return {
-    "bare_url": [baseurl, urlname, servicecrs$2].join(""),
-    "url": [baseurl, urlname, servicecrs$2, "/{z}/{x}/{y}.", format].join(""),
-    "format": format,
-    "minZoom": minZoom,
-    "maxZoom": maxZoom,
-    "attribution": attr$2,
-    "name": (name === 'luchtfoto' ? '' : 'NLMaps ') + ' ' + name
-  };
-}
-
-var PROVIDERS$2 = {
-  "standaard": makeProvider$2("standaard", "png", 6, 19),
-  "pastel": makeProvider$2("pastel", "png", 6, 19),
-  "grijs": makeProvider$2("grijs", "png", 6, 19),
-  "luchtfoto": makeProvider$2("luchtfoto", "jpeg", 6, 19)
-};
-
-/*
- *  * Get the named provider, or throw an exception if it doesn't exist.
- *   */
-function getProvider$2(name) {
-  if (name in PROVIDERS$2) {
-    var provider = PROVIDERS$2[name];
-
-    if (provider.deprecated && console && console.warn) {
-      console.warn(name + " is a deprecated style; it will be redirected to its replacement. For performance improvements, please change your reference.");
-    }
-
-    return provider;
-  } else {
-    console.error('NL Maps error: You asked for a style which does not exist! Available styles: ' + Object.keys(PROVIDERS$2).join(', '));
-  }
-}
-
-var geolocator_icon$2 = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" height="7.0556mm" width="7.0556mm" version="1.1" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" viewBox="0 0 24.999999 24.999999"> <metadata>  <rdf:RDF>   <cc:Work rdf:about="">    <dc:format>image/svg+xml</dc:format>    <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>    <dc:title/>   </cc:Work>  </rdf:RDF> </metadata> <g transform="translate(-151.39 -117.97)">  <g transform="translate(.39250 .85750)">   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m163.5 123.27c-3.4931 0-6.3379 2.8448-6.3379 6.3379s2.8448 6.3398 6.3379 6.3398 6.3379-2.8467 6.3379-6.3398-2.8448-6.3379-6.3379-6.3379zm0 1.3008c2.7905 0 5.0391 2.2466 5.0391 5.0371s-2.2485 5.0391-5.0391 5.0391c-2.7905 0-5.0391-2.2485-5.0391-5.0391 0-2.7905 2.2485-5.0371 5.0391-5.0371z"/>   <circle cx="163.5" cy="129.61" r="1.9312" style="fill:#191919"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m162.85 120.57v3.3555h1.3008v-3.3555h-1.3008z"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m162.85 135.3v3.3555h1.3008v-3.3555h-1.3008z"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m154.46 128.96v1.2988h3.3535v-1.2988h-3.3535z"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m169.19 128.96v1.2988h3.3535v-1.2988h-3.3535z"/>  </g> </g></svg>';
-
-var _typeof$3 = typeof Symbol === "function" && _typeof$1(Symbol.iterator) === "symbol" ? function (obj) {
-  return typeof obj === 'undefined' ? 'undefined' : _typeof$1(obj);
-} : function (obj) {
-  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === 'undefined' ? 'undefined' : _typeof$1(obj);
-};
-
 var asyncGenerator$2 = function () {
   function AwaitValue(value) {
     this.value = value;
@@ -891,6 +757,300 @@ var set$2 = function set$2(object, property, value, receiver) {
 
     if (parent !== null) {
       set$2(parent, property, value, receiver);
+    }
+  } else if ("value" in desc && desc.writable) {
+    desc.value = value;
+  } else {
+    var setter = desc.set;
+
+    if (setter !== undefined) {
+      setter.call(receiver, value);
+    }
+  }
+
+  return value;
+};
+
+function bgLayer$1() {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'standaard';
+
+  var provider = getProvider$1(name);
+  if ((typeof ol === 'undefined' ? 'undefined' : _typeof$2(ol)) === "object") {
+    return new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: provider.url,
+        attributions: [new ol.Attribution({
+          html: provider.attribution
+        })]
+      })
+    });
+  } else {
+    throw 'openlayers is not defined';
+  }
+}
+
+function geoLocatorControl$1(geolocator, map) {
+  var myControlEl = document.createElement('div');
+  myControlEl.id = 'nlmaps-geolocator-control';
+  myControlEl.style.backgroundColor = '#fff';
+  myControlEl.style.cursor = 'pointer';
+  myControlEl.style.boxShadow = '0 1px 5px rgba(0, 0, 0, 0.65)';
+  myControlEl.style.height = '26px';
+  myControlEl.style.width = '26px';
+  myControlEl.style.borderRadius = '26px 26px';
+  myControlEl.innerHTML = geolocator_icon$1;
+  myControlEl.className = 'ol-control';
+  myControlEl.style.right = '.5em';
+  myControlEl.style.top = '.5em';
+
+  myControlEl.addEventListener('click', function (e) {
+    geolocator.start();
+  });
+  function moveMap(d) {
+    var map = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : map;
+
+    var oldZoom = map.getView().getZoom();
+    var view = new ol.View({
+      center: ol.proj.fromLonLat([d.coords.longitude, d.coords.latitude]),
+      zoom: oldZoom
+    });
+    map.setView(view);
+  }
+  geolocator.on('position', function (d) {
+    moveMap(d, map);
+  });
+  var control = new ol.control.Control({ element: myControlEl });
+  return control;
+}
+
+/*parts copied from maps.stamen.com: https://github.com/stamen/maps.stamen.com/blob/master/js/tile.stamen.js
+ * copyright (c) 2012, Stamen Design
+ * under BSD 3-Clause license: https://github.com/stamen/maps.stamen.com/blob/master/LICENSE
+ */
+//https://geodata.nationaalgeoregister.nl/tiles/service/wmts/
+//https://geodata.nationaalgeoregister.nl/luchtfoto/rgb/wmts/
+
+var lufostring$2 = 'luchtfoto/rgb';
+var brtstring$2 = 'tiles/service';
+var servicecrs$2 = '/EPSG:3857';
+var attr$2 = 'Kaartgegevens &copy; <a href="kadaster.nl">Kadaster</a> | <a href="http://www.verbeterdekaart.nl">verbeter de kaart</a>';
+function baseUrl$2(name) {
+  return 'https://geodata.nationaalgeoregister.nl/' + (name === 'luchtfoto' ? lufostring$2 : brtstring$2) + '/wmts/';
+}
+
+function mapLayerName$2(layername) {
+  var name = void 0;
+  switch (layername) {
+    case 'standaard':
+      name = 'brtachtergrondkaart';
+      break;
+    case 'grijs':
+      name = 'brtachtergrondkaartgrijs';
+      break;
+    case 'pastel':
+      name = 'brtachtergrondkaartpastel';
+      break;
+    case 'luchtfoto':
+      name = '2016_ortho25';
+      break;
+    default:
+      name = 'brtachtergrondkaart';
+  }
+  return name;
+}
+
+function makeProvider$2(name, format, minZoom, maxZoom) {
+  var baseurl = baseUrl$2(name);
+  var urlname = mapLayerName$2(name);
+  return {
+    "bare_url": [baseurl, urlname, servicecrs$2].join(""),
+    "url": [baseurl, urlname, servicecrs$2, "/{z}/{x}/{y}.", format].join(""),
+    "format": format,
+    "minZoom": minZoom,
+    "maxZoom": maxZoom,
+    "attribution": attr$2,
+    "name": (name === 'luchtfoto' ? '' : 'NLMaps ') + ' ' + name
+  };
+}
+
+var PROVIDERS$2 = {
+  "standaard": makeProvider$2("standaard", "png", 6, 19),
+  "pastel": makeProvider$2("pastel", "png", 6, 19),
+  "grijs": makeProvider$2("grijs", "png", 6, 19),
+  "luchtfoto": makeProvider$2("luchtfoto", "jpeg", 6, 19)
+};
+
+/*
+ *  * Get the named provider, or throw an exception if it doesn't exist.
+ *   */
+function getProvider$2(name) {
+  if (name in PROVIDERS$2) {
+    var provider = PROVIDERS$2[name];
+
+    if (provider.deprecated && console && console.warn) {
+      console.warn(name + " is a deprecated style; it will be redirected to its replacement. For performance improvements, please change your reference.");
+    }
+
+    return provider;
+  } else {
+    console.error('NL Maps error: You asked for a style which does not exist! Available styles: ' + Object.keys(PROVIDERS$2).join(', '));
+  }
+}
+
+var geolocator_icon$2 = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" height="7.0556mm" width="7.0556mm" version="1.1" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" viewBox="0 0 24.999999 24.999999"> <metadata>  <rdf:RDF>   <cc:Work rdf:about="">    <dc:format>image/svg+xml</dc:format>    <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>    <dc:title/>   </cc:Work>  </rdf:RDF> </metadata> <g transform="translate(-151.39 -117.97)">  <g transform="translate(.39250 .85750)">   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m163.5 123.27c-3.4931 0-6.3379 2.8448-6.3379 6.3379s2.8448 6.3398 6.3379 6.3398 6.3379-2.8467 6.3379-6.3398-2.8448-6.3379-6.3379-6.3379zm0 1.3008c2.7905 0 5.0391 2.2466 5.0391 5.0371s-2.2485 5.0391-5.0391 5.0391c-2.7905 0-5.0391-2.2485-5.0391-5.0391 0-2.7905 2.2485-5.0371 5.0391-5.0371z"/>   <circle cx="163.5" cy="129.61" r="1.9312" style="fill:#191919"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m162.85 120.57v3.3555h1.3008v-3.3555h-1.3008z"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m162.85 135.3v3.3555h1.3008v-3.3555h-1.3008z"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m154.46 128.96v1.2988h3.3535v-1.2988h-3.3535z"/>   <path style="color-rendering:auto;text-decoration-color:#000000;color:#000000;shape-rendering:auto;solid-color:#000000;text-decoration-line:none;fill:#191919;fill-rule:evenodd;mix-blend-mode:normal;block-progression:tb;text-indent:0;image-rendering:auto;white-space:normal;text-decoration-style:solid;isolation:auto;text-transform:none" d="m169.19 128.96v1.2988h3.3535v-1.2988h-3.3535z"/>  </g> </g></svg>';
+
+var _typeof$3 = typeof Symbol === "function" && _typeof$1(Symbol.iterator) === "symbol" ? function (obj) {
+  return typeof obj === 'undefined' ? 'undefined' : _typeof$1(obj);
+} : function (obj) {
+  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === 'undefined' ? 'undefined' : _typeof$1(obj);
+};
+
+var asyncGenerator$3 = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function wrap(fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function _await(value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
+
+var get$3 = function get$3(object, property, receiver) {
+  if (object === null) object = Function.prototype;
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+
+  if (desc === undefined) {
+    var parent = Object.getPrototypeOf(object);
+
+    if (parent === null) {
+      return undefined;
+    } else {
+      return get$3(parent, property, receiver);
+    }
+  } else if ("value" in desc) {
+    return desc.value;
+  } else {
+    var getter = desc.get;
+
+    if (getter === undefined) {
+      return undefined;
+    }
+
+    return getter.call(receiver);
+  }
+};
+
+var set$3 = function set$3(object, property, value, receiver) {
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+
+  if (desc === undefined) {
+    var parent = Object.getPrototypeOf(object);
+
+    if (parent !== null) {
+      set$3(parent, property, value, receiver);
     }
   } else if ("value" in desc && desc.writable) {
     desc.value = value;

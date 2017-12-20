84871f5049536cb7c2d28b6a19683fa942bdc941
shoek <s.hoek@geogap.nl>
2017-12-20 15:25:57 +0100

    added rijksoverheid marker to leaflet

--- packages/nlmaps-leaflet/src/index.js
+++ packages/nlmaps-leaflet/src/index.js
@@ -223,7 +223,13 @@ function markerLayer(latLngObject) {
       lat = latLngObject.latitude;
       lng = latLngObject.longitude;
     }
-    return new L.marker([lat, lng]);
+    return new L.marker([lat, lng], {
+      icon: new L.icon({
+        iconUrl: 'https://rawgit.com/webmapper/nlmaps/master/dist/assets/rijksoverheid-marker.png',
+        iconSize: [64, 64],
+        iconAnchor: [32, 63]
+      })
+    });
   }
 }
 
@@ -260,13 +266,13 @@ function getMapCenter(map) {
 }
 
 // Until the building works properly, this is here. Should be in browser-test.js /// 
-// var map = L.map('map').setView([52, 5], 10);
-// var standaard = bgLayer('pastel');
-// const overlay = overlayLayer('gebouwen');
-// const marker = markerLayer();
+var map = L.map('map').setView([52, 5], 10);
+var standaard = bgLayer('pastel');
+const overlay = overlayLayer('gebouwen');
+const marker = markerLayer();
 
-// standaard.addTo(map);
-// overlay.addTo(map);
-// marker.addTo(map);
-// L.geocoderControl(geocoder).addTo(map);
+standaard.addTo(map);
+overlay.addTo(map);
+marker.addTo(map);
+L.geocoderControl(geocoder).addTo(map);
 export { bgLayer, overlayLayer, markerLayer, getMapCenter, geoLocatorControl, geocoderControl};

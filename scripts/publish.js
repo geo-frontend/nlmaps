cb7b5f27c9887b221858995ce18d7c1611b890d0
Hans Fast <hans@surfacemarkup.net>
2018-01-02 13:36:57 +0100

    add publish script

---
+++ scripts/publish.js
@@ -0,0 +1,19 @@
+const shell = require('shelljs');
+const helpers = require('./helpers');
+
+const tasks = helpers.tasks();
+
+function main() {
+  //copy updated compiled files to top-level 'dist' directory
+  tasks.forEach(task => {
+    console.log('copying '+ task + '...')
+    shell.cp('-u', 'packages/' + helpers.packagePath(task) + '/build/' + helpers.packagePath(task) + '*.js', 'dist/')
+  })
+}
+
+
+if (typeof require !== 'undefined' && require.main === module) {
+    main();
+}
+
+module.exports = main; 

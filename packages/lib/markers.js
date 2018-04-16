bd30a46abd84d01dbd9bf8560cc8d26b5e35dc46
Hans Fast <hans@surfacemarkup.net>
2018-04-16 15:13:08 +0200

    apply my code from stash

---
+++ packages/lib/markers.js
@@ -0,0 +1,40 @@
+let marker = {};
+
+function singleClick(e) {
+  if (marker.marker) {
+    marker.marker.remove();
+    if (spatialEq(marker.marker._latlng, e )) {
+      return;
+    }
+  }
+  let newmarker = L.marker([e.lat,e.lng]);
+  marker.marker = newmarker;
+  marker.marker.addTo(map);
+  let button = document.createElement('button');
+  button.innerHTML = 'remove'
+  button.addEventListener('click',function(e) {
+    marker.marker.remove();
+    delete marker.marker;
+  })
+  marker.marker.bindPopup(button);
+
+
+}
+
+function multiClick(e) {
+  if (marker.markers && marker.markers.length > 0) {
+    let hasSameLoc = marker.markers.find(el => spatialEq(el._latlng, e)) // any one has same location as new click?  
+    if (typeof hasSameLoc !== 'undefined') {
+      return;
+    }
+
+  }
+}
+
+function spatialEq(o, n){
+ if (distance([o.lat, o.lng],[n.lat, n.lng], {units: 'kilometers'}) < 0.001) {
+  return true;
+ } else {
+  return false;
+ }
+}
--- packages/nlmaps/test/browser-test.js
+++ packages/nlmaps/test/browser-test.js
@@ -1,13 +1,6 @@
-//import {bgLayer, geoLocatorControl} from '../../nlmaps-leaflet/build/nlmaps-leaflet.es.js';
+let map = nlmaps.createMap({target: 'mapdiv'})
 
-//let map = L.map('mapdiv').setView([52, 5], 10);
-//let layers = {
-  //standaard: bgLayer(), 
-//};
-//layers.standaard.addTo(map);
-//layers.pastel = bgLayer('pastel');
-//layers.grijs = bgLayer('grijs');
-//L.control.layers(layers).addTo(map);
-//let geo = geolocator();
-//geoLocatorControl(geo).addTo(map);
 
+nlmaps.clickprovider(map).subscribe(singleClick);
+
+nlmaps.clickprovider(map).subscribe(e => console.log(e))

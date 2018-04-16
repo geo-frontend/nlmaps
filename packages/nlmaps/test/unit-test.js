95f5c21ed9d2eb2ac9c40824834b863ba287e5cc
Steven Ottens <github@minst.net>
2018-04-16 16:21:02 +0200

    tape + babel support

--- packages/nlmaps/test/unit-test.js
+++ packages/nlmaps/test/unit-test.js
@@ -1,14 +1,27 @@
-let tap = require('../../../node_modules/tap');
-
+let test = require('../../../node_modules/tape');
+const shell = require('shelljs');
 
 //makeMap should be default?
-tap.test('top-level nlmaps package exports makeMap, leaflet, googlemaps and openlayers', function(t){
+test('top-level nlmaps package exports makeMap, leaflet, googlemaps and openlayers', function(t){
   let nlmaps = require('../build/nlmaps.cjs.js').nlmaps;
-  t.assert(typeof nlmaps.createMap === 'function');
-  t.assert(typeof nlmaps.leaflet === 'object');
-  t.assert(typeof nlmaps.googlemaps === 'object');
-  t.assert(typeof nlmaps.openlayers === 'object');
-  t.assert(typeof nlmaps.geoLocate === 'function');
+  t.equal(typeof nlmaps.createMap, 'function');
+  t.equal(typeof nlmaps.leaflet , 'object');
+  t.equal(typeof nlmaps.googlemaps, 'object');
+  t.equal(typeof nlmaps.openlayers , 'object');
+  t.equal(typeof nlmaps.geoLocate , 'function');
   t.end();
 
+});
+
+//test configPArser
+test('config parser',function(t){
+  const TEMPCONFDIR = 'packages/config/.tmp';
+  if (!shell.test('-e', TEMPCONFDIR)) {
+    shell.mkdir(TEMPCONFDIR);
+  }
+  shell.cp('packages/nlmaps/test/testconfig.js', TEMPCONFDIR + '/config.js');
+  let config = require('../../lib/configParser.js').CONFIG;
+  t.equal(typeof config.BASEMAP_PROVIDERS, 'object')
+  t.assert(config.BASEMAP_PROVIDERS.standaard !== undefined);
+  t.end();
 });

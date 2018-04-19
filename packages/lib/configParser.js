cd98d834b6f064546a7f2ae8ca2cfabbd4a60091
Steven Ottens <github@minst.net>
2018-04-19 12:00:07 +0200

    TMS

--- packages/lib/configParser.js
+++ packages/lib/configParser.js
@@ -70,7 +70,16 @@ function parseMap(map) {
 }
 
 function formatBasemapUrl(layer) {
-  layer.url = `${layer.url}/${layer.type}/${layer.urlname}/${CONFIG.BASE_DEFAULTS.crs}/{z}/{x}/{y}.${layer.format}`;
+    switch(layer.type){
+        case 'wmts':
+            layer.url = `${layer.url}/${layer.type}/${layer.urlname}/${layer.crs}/{z}/{x}/{y}.${layer.format}`;
+        break;
+        case 'tms':
+            layer.url = `${layer.url}/${layer.urlname}/{z}/{x}/{y}.${layer.format}`;
+        break;
+        default:
+            layer.url = `${layer.url}/${layer.type}/${layer.urlname}/${layer.crs}/{z}/{x}/{y}.${layer.format}`;
+    }  
   return layer;
 
 }

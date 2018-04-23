d20f1322c25bdd466991f034ccc5b051e2d35675
Hans Fast <hans@surfacemarkup.net>
2018-04-23 12:34:29 +0200

    refactor featurequery to take formatter functions for request and response
    this makes it generic and moves the RD handling to client.
    
    It still uses internal configured base url for query requests.

--- packages/lib/featurequery.js
+++ packages/lib/featurequery.js
@@ -1,4 +1,3 @@
-import proj4 from 'proj4';
 import { CONFIG } from './configParser.js';
 function query(url) {
   const promise = new Promise((resolve, reject) => {
@@ -10,32 +9,25 @@ function query(url) {
 
 }
 
-proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
-
-const transformCoords = proj4(proj4.defs('EPSG:4326'), proj4.defs('EPSG:28992'));
-
-function formatQueryURL(baseUrl, xy) {
-  return `${baseUrl}${xy.x},${xy.y},10`
-}
-
-function handleQueryResponse(res, d) {
-  let filtered = res.results.filter(x => x.hoofdadres === true);
-  if (filtered.length > 0) {
-    return { queryResult: filtered[0], latlng: d.latlng };
-  } else {
-    return { queryResult: null, latlng: d.latlng }
-  }
-}
 
 //transforming operator
-const pointToQuery = url => inputSource => {
+//returns an object with original latlng and queryResult:
+// {
+//   queryResult: {},
+//   latlng: d.latlng
+// }
+// user-supplied responseFormatter is used to create queryResult.
+const pointToQuery = (url, requestFormatter, responseFormatter) => inputSource => {
   return function outputSource (start, outputSink) {
     if (start !== 0 ) return;
     inputSource(0, (t, d) => {
       if (t === 1) {
-        let xyRD = transformCoords.forward({x: d.latlng.lng, y: d.latlng.lat});
-        query(formatQueryURL(url, xyRD)).then(res => {
-          let output = handleQueryResponse(res, d);
+        let queryUrl = requestFormatter(url, {x: d.latlng.lng, y: d.latlng.lat});
+        query(queryUrl).then(res => {
+          let output =  {
+            queryResult: responseFormatter(res),
+            latlng: d.latlng
+          }
           outputSink(1, output);
         })
       } else {
@@ -46,8 +38,9 @@ const pointToQuery = url => inputSource => {
 }
 
 //constructor to create a 'clickpricker' in one go.
-const queryFeatures = function(source) {
-  const querier =  pointToQuery(CONFIG.FEATUREQUERYBASEURL)(source);
+const queryFeatures = function(source, requestFormatter, responseFormatter) {
+  let URL = CONFIG.FEATUREQUERYBASEURL;
+  const querier =  pointToQuery(URL, requestFormatter, responseFormatter)(source);
   querier.subscribe = function(callback) {
     querier(0, callback)
   }
--- packages/nlmaps/test/browser-test.js
+++ packages/nlmaps/test/browser-test.js
@@ -4,14 +4,33 @@ let map = nlmaps.createMap({target: 'mapdiv', marker: true, search: true})
 
 function myHandler(t, d) {
  if (t === 1 ){
-   console.log('my special handler says: ')
+   console.log('The click happened at: ')
   console.log(d);
   }
 }
 
 let clicks = nlmaps.clickprovider(map);
-let singleMarker = nlmaps.singleClick(map);
-let featureQuery = nlmaps.queryFeatures(clicks);
+//let singleMarker = nlmaps.singleClick(map, markerCreator);
+let singleMarker = nlmaps.singleClick(map)
+
+
+proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
+
+const transformCoords = proj4(proj4.defs('EPSG:4326'), proj4.defs('EPSG:28992'));
+
+function requestFormatter(baseUrl, xy) {
+  let xyRD = transformCoords.forward(xy);
+  return `${baseUrl}${xyRD.x},${xyRD.y},10`
+}
+
+function responseFormatter(res) {
+  let filtered = res.results.filter(x => x.hoofdadres === true);
+  return filtered.length > 0 ? filtered[0] : null;
+}
+
+
+let featureQuery = nlmaps.queryFeatures(clicks, requestFormatter, responseFormatter);
+
 
 featureQuery.subscribe(singleMarker)
 

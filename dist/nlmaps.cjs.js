c65ebdee2b3d2390f86277a3f4b81d989a4adfad
CircleCI <github@minst.net>
2018-10-25 14:29:39 +0000

    Tagged version v2.3.51

--- dist/nlmaps.cjs.js
+++ dist/nlmaps.cjs.js
@@ -37,7 +37,7 @@ var _descriptors = !_fails(function () {
 });
 
 var _core = createCommonjsModule(function (module) {
-  var core = module.exports = { version: '2.5.6' };
+  var core = module.exports = { version: '2.5.7' };
   if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
 });
 var _core_1 = _core.version;
@@ -6700,7 +6700,7 @@ var config = {
             "layerName": "pand"
         }, {
             "name": "percelen",
-            "workSpaceName": "bkadastralekaartv3ag",
+            "workSpaceName": "kadastralekaartv3",
             "layerName": "kadastralekaart"
         }, {
             "name": "drone-no-fly-zones",
--- dist/nlmaps.es.js
+++ dist/nlmaps.es.js
@@ -33,7 +33,7 @@ var _descriptors = !_fails(function () {
 });
 
 var _core = createCommonjsModule(function (module) {
-  var core = module.exports = { version: '2.5.6' };
+  var core = module.exports = { version: '2.5.7' };
   if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
 });
 var _core_1 = _core.version;
@@ -6696,7 +6696,7 @@ var config = {
             "layerName": "pand"
         }, {
             "name": "percelen",
-            "workSpaceName": "bkadastralekaartv3ag",
+            "workSpaceName": "kadastralekaartv3",
             "layerName": "kadastralekaart"
         }, {
             "name": "drone-no-fly-zones",

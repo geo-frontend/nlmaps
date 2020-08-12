9586520d6e4dccdf47064a29d3aec683011487c1
SjorsHoek <S.hoek@geogap.nl>
2020-08-12 13:55:27 +0200

    update test with new rollup packages

--- packages/nlmaps-leaflet/config/rollup.test.js
+++ packages/nlmaps-leaflet/config/rollup.test.js
@@ -1,9 +1,9 @@
 // Rollup plugins
-import babel from '../../../node_modules/rollup-plugin-babel/dist/rollup-plugin-babel.es.js';
+import babel from '../../../node_modules/@rollup/plugin-babel/dist/index.es.js';
 import {eslint} from 'rollup-plugin-eslint';
-import resolve from '../../../node_modules/rollup-plugin-node-resolve/dist/rollup-plugin-node-resolve.es.js';
-import commonjs from '../../../node_modules/rollup-plugin-commonjs/dist/rollup-plugin-commonjs.es.js';
-import replace from '../../../node_modules/rollup-plugin-replace/dist/rollup-plugin-replace.es.js';
+import resolve from '../../../node_modules/@rollup/plugin-node-resolve/dist/es/index.js';
+import commonjs from '../../../node_modules/@rollup/plugin-commonjs/dist/index.es.js';
+import replace from '../../../node_modules/@rollup/plugin-replace/dist/rollup-plugin-replace.es.js';
 import uglify from 'rollup-plugin-uglify';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';

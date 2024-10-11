c7d8a54555511cf4f65e4fffb06408a9b346f675
Edward Mac Gillavry <edward@webmapper.net>
2024-10-11 13:42:31 +0200

    Use Vite to create the lib.

--- packages/nlmaps-geolocator/config/rollup.es.js
+++
@@ -1,9 +0,0 @@
-import config from './rollup.config';
-
-export default config({
-  output: {
-    format: 'es',
-    file: 'build/nlmaps-geolocator.es.js',
-  },
-  browser: false
-})
--- packages/nlmaps-geolocator/src/index.js
+++ packages/nlmaps-geolocator/src/index.js
@@ -3,53 +3,51 @@ const geoLocateDefaultOpts = {
 }
 
 /* eslint-disable-next-line no-unused-vars */
-import emitonoff from 'emitonoff';
+import emitonoff from 'emitonoff'
 
 function positionHandler(position) {
-  this.emit('position', position);
-
+  this.emit('position', position)
 }
 function positionErrorHandler(error) {
-  this.emit('error', error);
-
+  this.emit('error', error)
 }
 
 const GeoLocator = function (opts) {
-  const state = Object.assign({}, geoLocateDefaultOpts, opts);
+  const state = Object.assign({}, geoLocateDefaultOpts, opts)
 
   return {
-    start () {
-      state.started = true;
-      navigator.geolocation.getCurrentPosition(positionHandler.bind(this), positionErrorHandler.bind(this), {maximumAge: 60000})
-      return this;
+    start() {
+      state.started = true
+      navigator.geolocation.getCurrentPosition(positionHandler.bind(this), positionErrorHandler.bind(this), { maximumAge: 60000 })
+      return this
     },
-    stop () {
-      state.started = false;
-      return this;
+    stop() {
+      state.started = false
+      return this
     },
     isStarted() {
-      return state.started;
+      return state.started
     },
-    log (){
+    log() {
       // eslint-disable-next-line no-console
-      console.log(state);
-      return this;
+      console.log(state)
+      return this
     }
-  };
-};
-
-function geoLocator(opts){
-  let navigator = typeof window !== 'undefined' ? window.navigator || {}: {};
-  if (typeof navigator !== 'undefined' && 'geolocation' in navigator) {
-    let geolocator = emitonoff(GeoLocator(opts));
-    geolocator.on('position', function() {
-      this.stop();
-    });
-    return geolocator;
-  } else {
-    let error = 'geolocation is not available in your browser.'
-    throw error;
   }
 }
 
-export default geoLocator;
+function geoLocator(opts) {
+  let navigator = typeof window !== 'undefined' ? window.navigator || {} : {}
+  if (typeof navigator !== 'undefined' && 'geolocation' in navigator) {
+    let geolocator = emitonoff(GeoLocator(opts))
+    geolocator.on('position', function () {
+      this.stop()
+    })
+    return geolocator
+  } else {
+    let error = 'geolocation is not available in your browser.'
+    throw error
+  }
+}
+
+export default geoLocator

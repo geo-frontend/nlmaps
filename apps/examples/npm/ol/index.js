cb1acd7cd691d443ef2be712881e014696a73c7b
Edward Mac Gillavry <edward@webmapper.net>
2024-12-20 17:16:57 +0100

    Extend OL example with geolocator control.

--- apps/examples/npm/ol/index.js
+++ apps/examples/npm/ol/index.js
@@ -1,7 +1,8 @@
 import Map from 'ol/Map.js'
 import View from 'ol/View.js'
 import { fromLonLat } from 'ol/proj.js'
-import { bgLayer, overlayLayer, markerLayer } from 'nlmaps-ol'
+import { bgLayer, geoLocatorControl, markerLayer, overlayLayer } from 'nlmaps-ol'
+import geoLocator from 'nlmaps-geolocator'
 
 import 'ol/ol.css'
 
@@ -16,7 +17,9 @@ const map = new Map({
 let layer = bgLayer()
 let overlay = overlayLayer('gemeenten')
 let marker = markerLayer({ longitude: 5.5, latitude: 52.5 })
-
+let geo = geoLocator()
+let geolocator = geoLocatorControl(geo, map)
 map.addLayer(layer)
 map.addLayer(overlay)
 map.addLayer(marker)
+map.addControl(geolocator)

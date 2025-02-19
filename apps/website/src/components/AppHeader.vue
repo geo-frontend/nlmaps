329645e31fbbe4aab7c216d25f88d0df5bf92779
Edward Mac Gillavry <edward@webmapper.net>
2025-02-19 14:51:45 +0100

    Responsive header container.

--- apps/website/src/components/AppHeader.vue
+++ apps/website/src/components/AppHeader.vue
@@ -16,8 +16,35 @@ header .container {
   justify-content: space-between;
   padding-left: 20px;
   padding-right: 20px;
-  max-width: 1140px;
+  margin-right: auto;
+  margin-left: auto;
 }
+
+header .container:before,
+header .container:after {
+  display: table;
+  content: ' ';
+}
+
+header .container:after {
+  clear: both;
+}
+@media (min-width: 768px) {
+  header .container {
+    width: 760px;
+  }
+}
+@media (min-width: 992px) {
+  header .container {
+    width: 980px;
+  }
+}
+@media (min-width: 1200px) {
+  header .container {
+    width: 1180px;
+  }
+}
+
 .logo-kadaster polyline,
 .logo-kadaster path {
   fill: #fff;

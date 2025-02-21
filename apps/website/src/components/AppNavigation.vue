52f17bd7a171e34144b966647bbdef8f146d05ba
Edward Mac Gillavry <edward@webmapper.net>
2025-02-21 15:58:54 +0100

    Responsive navigation.

--- apps/website/src/components/AppNavigation.vue
+++ apps/website/src/components/AppNavigation.vue
@@ -3,41 +3,62 @@
     <div class="section-intro">
       <h2 class="heading section-heading">Handige links</h2>
     </div>
-    <ul>
-      <li><a href="https://www.kadaster.nl" target="_blank">Kadaster</a></li>
-      <li><a href="https://pdok.nl" target="_blank">PDOK</a></li>
-      <li><a href="https://geoforum.nl/" target="_blank">PDOK Forum</a></li>
-      <li>
+    <ul class="list">
+      <li class="list-item">
+        <a href="https://www.kadaster.nl" target="_blank" class="list-link"
+          >Kadaster</a
+        >
+      </li>
+      <li class="list-item">
+        <a href="https://pdok.nl" target="_blank" class="list-link">PDOK</a>
+      </li>
+      <li class="list-item">
+        <a href="https://geoforum.nl/" target="_blank" class="list-link"
+          >PDOK Forum</a
+        >
+      </li>
+      <li class="list-item">
         <a
           href="https://www.kadaster.nl/zakelijk/producten/geo-informatie/nl-maps"
           target="_blank"
+          class="list-link"
           >Kadaster NL Maps</a
         >
       </li>
-      <li>
-        <a href="https://app.pdok.nl/viewer" target="_blank">PDOK Viewer</a>
+      <li class="list-item">
+        <a href="https://app.pdok.nl/viewer" target="_blank" class="list-link"
+          >PDOK Viewer</a
+        >
       </li>
-      <li>
-        <a href="https://www.pdok.nl/diensten#PDOK%20Fair%20Use" target="_blank"
+      <li class="list-item">
+        <a
+          href="https://www.pdok.nl/diensten#PDOK%20Fair%20Use"
+          target="_blank"
+          class="list-link"
           >PDOK fair-use</a
         >
       </li>
-      <li>
-        <a href="https://github.com/geo-frontend/nlmaps/" target="_blank"
+      <li class="list-item">
+        <a
+          href="https://github.com/geo-frontend/nlmaps/"
+          target="_blank"
+          class="list-link"
           >NL Maps op Github</a
         >
       </li>
-      <li>
+      <li class="list-item">
         <a
           href="https://www.toegankelijkheidsverklaring.nl/register/2808"
           target="_blank"
+          class="list-link"
           >Toegankelijkheid</a
         >
       </li>
-      <li>
+      <li class="list-item">
         <a
           href="https://github.com/geo-frontend/nlmaps/blob/develop/PRIVACY.md"
           target="_blank"
+          class="list-link"
           >Privacy</a
         >
       </li>
@@ -45,20 +66,18 @@
   </div>
 </template>
 <style lang="css" scoped>
-ul {
+.list {
   margin: 0 -20px;
   padding: 0;
-  display: flex;
+  -webkit-flex-wrap: wrap;
+  -ms-flex-wrap: wrap;
   flex-wrap: wrap;
-  max-width: 84em;
-}
-li {
-  text-align: left;
   list-style: none;
-  width: 30%;
+}
+.list-item {
   padding: 0 20px;
 }
-a {
+.list-link {
   display: block;
   padding: 12px 0;
   margin-top: -1px;
@@ -69,12 +88,21 @@ a {
     background 0.3s linear,
     padding 0.3s linear;
 }
-
-a:focus,
-a:hover {
+.list-link:focus,
+.list-link:hover {
   padding: 12px;
   text-decoration: none;
   background: #fff;
   color: #006486;
+}
+@media (min-width: 48em) {
+  .list {
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+  }
+  .list-item {
+    width: 33.33333%;
+  }
 }
 </style>

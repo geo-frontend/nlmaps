dae4e15e0ead806e6b3b542f6a829fe31f7a8289
Edward Mac Gillavry <edward@webmapper.net>
2025-02-24 15:08:02 +0100

    Use HTML5 Mode in Router.

--- apps/website/src/main.js
+++ apps/website/src/main.js
@@ -1,20 +1,19 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
 const routes = [
-    { path: "/", component: () => import("@/pages/HomePage.vue") },
-    { path: "/docs", component: () => import("@/pages/DocsPage.vue") },
-  ];
-  
-  const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-  });
-  
-  const app = createApp(App);
-  
-  app.use(router);
-  
-  app.mount("#app");
+  { path: '/', component: () => import('@/pages/HomePage.vue') },
+  { path: '/docs', component: () => import('@/pages/DocsPage.vue') }
+]
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')

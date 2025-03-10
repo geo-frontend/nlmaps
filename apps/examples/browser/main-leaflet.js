b6269da768e0a91cd3163b78fc64a5dbf587d916
Edward Mac Gillavry <edward@webmapper.net>
2025-03-10 11:56:30 +0100

    Remove attribution prefix explicitly.

--- apps/examples/browser/main-leaflet.js
+++ apps/examples/browser/main-leaflet.js
@@ -1,4 +1,6 @@
 var map = L.map('map-div').setView([52, 5], 10)
+map.attributionControl.setPrefix('')
+
 /* eslint-disable no-unused-vars */
 var layer = bgLayer().addTo(map)
 /* eslint-enable no-unused-vars */

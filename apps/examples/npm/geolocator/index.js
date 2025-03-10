e71b6e21419afa487e8331c3dbf66d3166a0d307
Edward Mac Gillavry <edward@webmapper.net>
2025-03-10 11:57:44 +0100

    Remove attribution prefix explicitly. Only import Map object from Leaflet package.

--- apps/examples/npm/geolocator/index.js
+++ apps/examples/npm/geolocator/index.js
@@ -1,10 +1,11 @@
-import L from 'leaflet'
+import { Map } from 'leaflet'
 import { bgLayer, geoLocatorControl } from 'nlmaps-leaflet'
 import geoLocator from 'nlmaps-geolocator'
 
 import 'leaflet/dist/leaflet.css'
 
-var map = L.map('map-div').setView([52, 5], 10)
+var map = new Map('map-div').setView([52, 5], 10)
+map.attributionControl.setPrefix('')
 
 /* eslint-disable no-unused-vars */
 var layer = bgLayer().addTo(map)
--- apps/examples/npm/leaflet/index.js
+++ apps/examples/npm/leaflet/index.js
@@ -1,9 +1,10 @@
-import L from 'leaflet'
+import { Map } from 'leaflet'
 import { bgLayer, geocoderControl } from 'nlmaps-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
-var map = L.map('map-div').setView([52, 5], 10)
+var map = new Map('map-div').setView([52, 5], 10)
+map.attributionControl.setPrefix('')
 
 /* eslint-disable no-unused-vars */
 var layer = bgLayer().addTo(map)

9c0f492781a77b7738b44a7eef6cf1afdc27a9d0
Edward Mac Gillavry <edward@webmapper.net>
2025-03-11 15:15:29 +0100

    Remove iife export for OL and remove comments.

--- packages/nlmaps-ol/vite.config.js
+++ packages/nlmaps-ol/vite.config.js
@@ -2,27 +2,20 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: __dirname,
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
     outDir: '../../dist',
     emptyOutDir: false,
     reportCompressedSize: true,
     commonjsOptions: {
-      transformMixedEsModules: true
+      transformMixedEsModules: true,
     },
     lib: {
-      // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.js',
-      name: 'nlmapsol',
       fileName: (format) => `nlmaps-ol.${format}.js`,
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs', 'iife']
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: []
-    }
-  }
+      external: [],
+    },
+  },
 })

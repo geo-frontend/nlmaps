b2aa1418dcef357636785fb4b3ecbd18358e9a9c
Edward Mac Gillavry <edward@webmapper.net>
2025-03-13 15:38:18 +0100

    Expand example to show custom WMS layer.

--- apps/examples/browser/main-leaflet.js
+++ apps/examples/browser/main-leaflet.js
@@ -4,3 +4,12 @@ map.attributionControl.setPrefix('')
 /* eslint-disable no-unused-vars */
 var layer = bgLayer().addTo(map)
 /* eslint-enable no-unused-vars */
+
+const endpoint = {
+  url: 'https://service.pdok.nl/ez/fysischgeografischeregios/wms/v1_0?',
+  layerName: 'fysischgeografischeregios',
+  styleName: 'fysischgeografischeregios',
+}
+const overlay = overlayLayer('fysisch-geografische-regios', endpoint)
+overlay.setOpacity(0.7)
+overlay.addTo(map)

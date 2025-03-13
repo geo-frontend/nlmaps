a026a2f7d03935c8b7152f7d301257ca9730a68b
Edward Mac Gillavry <edward@webmapper.net>
2025-03-13 11:46:14 +0100

    Extend the example for using the main nlmaps library.

--- apps/examples/browser/main-nlmaps.js
+++ apps/examples/browser/main-nlmaps.js
@@ -1,2 +1,11 @@
-/* eslint-disable no-unused-vars */
-let map = nlmaps.createMap({ target: 'map-div' })
+const mapOptions = {
+  target: 'map-div',
+  center: { latitude: 52.379189, longitude: 4.899431 },
+  zoom: 10,
+  style: 'pastel',
+  search: true,
+  marker: true,
+  overlay: 'gemeenten',
+}
+
+const map = nlmaps.createMap(mapOptions)
